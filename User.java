1546449765b31a1480c74b0ca313070e61efb61f
Hutton <ahutton1@urmc-sh.rochester.edu>
2020-06-23 16:07:59 -0400

    Created a new employmentStatus enum and moved all enums to their own package. Also created the rough skeletons for the User and Asset object classes. These objects are currently sitting in the main package. Likely will have to be moved to a separate object class

---
+++ User.java
@@ -0,0 +1,14 @@
+/*  @ahutton1 on github.com
+    Software created is for use by University of Rochester Medicine Home Care, and is not for outside use
+*/
+import enums.*;
+
+//Class that represents a user of the URMHC system
+public class User {
+    //TODO flush out user class variables
+    private String lastName, firstName, deptCode, manager, email;
+}
+
+public User(){
+    //TODO
+}
---
+++ enums/employmentStatus.java
@@ -0,0 +1,10 @@
+package enums;
+/*  @ahutton1 on github.com
+    Software created is for use by University of Rochester Medicine Home Care, and is not for outside use
+*/
+
+/*  Class that represents the different status' that an employee of URMHC can be
+*/
+public enum employmentStatus {
+    ACTIVE, LOA, TERMINATED;
+}
