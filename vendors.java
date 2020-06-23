89afc5871221c16339422a56acf6ae579ad791b7
Hutton <ahutton1@urmc-sh.rochester.edu>
2020-06-23 15:26:03 -0400

    Enums added. Still TODOs for double checking to make sure that all are accounted for, however the general skeleton for most of the object types have been added.

--- vendors.java
+++ vendors.java
@@ -1,3 +1,9 @@
+/*  @ahutton1 on github.com
+    Software created is for use by University of Rochester Medicine Home Care, and is not for outside use
+*/
+
+/*  Enum class that defines the different vendors that assets can be purchased from. Computers utilize DELL through HP, Aircards and hotspots utilize VERIZON and ATandT
+*/
 public enum vendors {
-    
+    DELL, PANASONIC, LENOVO, HP, VERIZON, ATandT;
 }
