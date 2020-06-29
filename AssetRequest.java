fedf05fd8d6d88b725e6e46e9892c5a3bfe2285b
Hutton <ahutton1@urmc-sh.rochester.edu>
2020-06-29 16:11:31 -0400

    Added the SQL statement handler class skeleton. The class will handle translations from the java inputs and data into SQL statments that will be able to be read by the SQL server

--- AssetRequest.java
+++ AssetRequest.java
@@ -2,8 +2,12 @@ import java.io.Serializable;
 
 public class AssetRequest<E extends Serializable> implements Serializable{
 
+    /**
+     * Handles the types of requests that can be sent back and forth between the clients and the server
+     */
     public enum RequestType{
         /**
+         * CLIENT
          * Used by the client application to request a connection to the server. The server will take the request
          * and store it in a special ArrayList as an IP address. This allows the server to treat the IP addresses 
          * as usernames, and keep track of any clients that are actively connected to the server.
@@ -11,6 +15,7 @@ public class AssetRequest<E extends Serializable> implements Serializable{
         LOGIN, 
         
         /**
+         * SERVER
          * Used by the server to approve and provide access to a inbound client 'LOGIN' request. After the request
          * is run and checked, the server sends this as a knock-knock protocol to affirmate that the client has
          * successfully connected
@@ -18,12 +23,14 @@ public class AssetRequest<E extends Serializable> implements Serializable{
         LOGIN_SUCCEEDED, 
         
         /**
+         * SERVER
          * Used by the server to notify all connected clients that a change was made to an active form and that they
          * need to receive updated versions of the forms.
          */
         PUSH_UPDATE, 
         
         /**
+         * CLIENT
          * Used by the client to notify the server that they are disconnecting and that the thread needs to be closed.
          * The server must receive this message and then close and remove the thread from the list of IP addresses
          * being stored in order for the client to be able to connect again at a later time with that same IP.
@@ -31,6 +38,7 @@ public class AssetRequest<E extends Serializable> implements Serializable{
         LOGOUT, 
         
         /**
+         * SERVER
          * Used by the server to notify all active users that a fatal error occurred and that the server is going
          * to be shut down. All connected users will be removed from the server and their applications will clsoe
          * upon receiving this message.
@@ -38,6 +46,7 @@ public class AssetRequest<E extends Serializable> implements Serializable{
         ERROR, 
         
         /**
+         * CLIENT
          * Used by the client to call the server and tell it that a new asset is being created. This will tell the 
          * server all of the information it needs to know in order to create a new asset in the SQL server asset
          * database.
@@ -45,15 +54,58 @@ public class AssetRequest<E extends Serializable> implements Serializable{
         NEW_ASSET, 
         
         /**
-         * Used by the clietn to call the server and tel it that a new user is being created. This will tell the 
+         * CLIENT
+         * Used by the client to call the server and tell it that a new user is being created. This will tell the 
          * server all of the information it needs to know in order to create a new user in the SQL server user
          * database.
          */
         NEW_USER, 
+
+        /**
+         * CLIENT
+         * Used by the client to call the server and request to view the in-depth information regarding a given asset.
+         * Sends the server the given asset search term and info necessary for the system to develop a SQL statement.
+         */
+        CALL_ASSET,
+
+        /**
+         * CLIENT
+         * Used by the client to call the server and request to view the indepth information regarding a given user.
+         * Sends the server the given user search term and info necessary for the system to develop a SQL statment.
+         */
+        CALL_USER,
+
+        /**
+         * CLIENT
+         * Used by the client to call the server and request a given list of assets. The request holds the user, the 
+         * asset type that they have selected within the list, and then any other information in order for the server
+         * to successfully create the necessary SQL statement.
+         */
+        CALL_ASSET_LIST,
+
+        /**
+         * CLIENT
+         * Used by the client to call the server and request a given list of users. The request holds the user, and
+         * the general request to get the basic information on all users. The server can then create a SQL statement
+         * based on this information and report back with the necessary information. 
+         */
+        CALL_USER_LIST
     }
 
-    public AssetRequest(){
+    private RequestType type;
+    private E data;
 
+    public AssetRequest(RequestType type, E data){
+        this.type = type;
+        this.data = data;
+    }
+
+    public RequestType getType(){return type;}
+
+    public E getData(){return data;}
+
+    public String toString(){
+        return "Server Request{ type = " + type + ", data = " + data + "}";
     }
     
 }
---
+++ sqlStatementHandler.java
@@ -0,0 +1,70 @@
+//TODO: ALL METHODS ARE STUBS. Methods needs to be fully flushed out and created based on information provided and needed SQL statements
+
+            //For Reference -> View SQL Textbook
+
+/**
+ * Helper class that assists in the formatting of SQL statements to and from the server as clients connect and
+ * interact with it. The handler class can take in given search conditions or general listings used in the GUI,
+ * and develop dynamic SQL statments from those terms.
+ */
+public class sqlStatementHandler {
+
+    /**
+     * Method that sends a SQL statement to the SQL virtual server requesting that a new asset be created and 
+     * added to the tables.
+     * @return true or false depending on whether the message was sent successfully
+     */
+    public boolean reqNewAsset(){
+        return true;
+    }
+
+    /**
+     * Method that sends a SQL statement to the SQL virtual server requesting that a new user be created and 
+     * added to the tables. Important note* -> Look into how ADUC could potentially have new users added (LOW 
+     * CRITICALITY)
+     * @return true or false depending on whether the message was sent successfully
+     */
+    public boolean reqNewUser(){
+        return true;
+    }
+
+    /**
+     * Method that sends a SQL statement to the SQL virtual server requesting the entire list of assets that
+     * is stored in the tables, based on which search filter or term combination and a user has on at any given
+     * time.
+     * @return true or false depending on whether the message was sent successfully
+     */
+    public boolean reqAssetList(){
+        return true;
+    }
+
+    /**
+     * Method that sends a SQL statement to the SQL virtual server requesting the entire list of useres that is
+     * stored in the tables, based on what search conditions a user has on at any given time.
+     * @return true or false depending on whether the message was sent successfully
+     */
+    public boolean reqUserList(){
+        return true;
+    }
+
+    /**
+     * Method that sends a SQL statement to the SQL virtual server requesting all of the information regarding
+     * a singular given user. That users' information is to be displayed in the main information display panel
+     * on the right hand side of the application screen.
+     * @return true or false depending on whether the message was sent successfully
+     */
+    public boolean reqUserInfo(){
+        return true;
+    }
+
+    /**
+     * Method that sends a SQL statement to the SQL virtual server requesting all of the information regarding
+     * a singular given asset. That asset's information is to be displayed in the main information display 
+     * panel on the right hand side of the application screen
+     * @return true or false depending on whether the message was sent successfully
+     */
+    public boolean reqAssetInfo(){
+        return true;
+    }
+    
+}
